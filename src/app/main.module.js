2f82d770ce61b55c36978852829bff1258500d1e
Alcino <steveneizy1294@gmail.com>
2019-07-23 21:04:13 -0300

    criou as routas de configuração de itens, clientes e ordem de servico

--- src/app/main.module.js
+++ src/app/main.module.js
@@ -4,7 +4,13 @@ import { default as uiRouter } from "@uirouter/angularjs";
 import diretivas from "./diretivas";
 
 import { mainConfig } from "./main/config";
+import { clienteConfig } from "./clientes/config";
+import { itemConfig } from "./itens/config";
+import { osConfig } from "./os/config";
 
 export default angular
   .module("app", [uiRouter, diretivas])
-  .config(mainConfig).name;
+  .config(mainConfig)
+  .config(osConfig)
+  .config(itemConfig)
+  .config(clienteConfig).name;
